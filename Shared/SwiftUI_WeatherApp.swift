39cf5c9630c7059ca39beb9fc53ff0dad2c93a60
David Thomason <dtom90@users.noreply.github.com>
2021-02-17 19:43:24 -0800

    Initial Commit

---
+++ Shared/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  Shared
+//
+//  Created by David Thomason on 2/17/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

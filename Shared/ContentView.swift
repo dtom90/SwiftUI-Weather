ae0d93161b61a1c65b7f793150e0e3b773312e29
David Thomason <dtom90@users.noreply.github.com>
2021-02-17 20:39:50 -0800

    Created basic elements

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,8 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.blue, .white]),
+                           startPoint: .topLeading,
+                           endPoint: .bottomTrailing)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                Text("Cupertino, CA")
+                    .font(.system(size: 32, weight: .medium, design: .default ))
+                    .foregroundColor(.white)
+                    .padding()
+                
+                VStack(spacing: 8) {
+                    
+                    Image(systemName: "cloud.sun.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 180, height: 180)
+                    
+                    Text("76°")
+                        .font(.system(size: 70, weight: .medium))
+                        .foregroundColor(.white)
+                    
+                }
+                Spacer()
+            }
+        }
     }
 }
 
